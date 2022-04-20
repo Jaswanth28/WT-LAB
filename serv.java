aec391dc21d71d7d973bd43f4ace9a7bf4a83f7e
Jaswanth <81845691+Jaswanth28@users.noreply.github.com>
2022-04-20 14:56:02 +0530

    Create serv.java

---
+++ serv.java
@@ -0,0 +1,47 @@
+//this is aservlet file 
+//creeated in serv ee
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class serv
+ */
+@WebServlet("/serv")
+public class serv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public serv() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw= response.getWriter();
+		String s=request.getParameter("t1");
+		pw.println("<h1> hello "+s+"</h1>");
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
